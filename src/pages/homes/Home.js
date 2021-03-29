9f7504215607ea391a148708966631d746c2c3c7
jpcarosuarez <46928724+jpcarosuarez@users.noreply.github.com>
2021-03-29 17:13:29 -0500

    Cambia Logo e inicia el proyecto Kiosko Inmobiliario

--- src/pages/homes/Home.js
+++ src/pages/homes/Home.js
@@ -1,74 +1,84 @@
-import React  from 'react'
-import GeneralHeader from '../../components/common/GeneralHeader'
-import BannerOne from '../../components/banner/banner1/BannerOne'
+import React from 'react';
+import GeneralHeader from "../../components/common/GeneralHeader";
+import BannerTwo from "../../components/banner/banner2/BannerTwo";
+import ScrollTopBtn from "../../components/common/ScrollTopBtn";
+import SectionsHeading from "../../components/common/SectionsHeading";
+import PopularDestination from "../../components/places/PopularDestination";
+import Button from "../../components/common/Button";
+import { BsEye } from 'react-icons/bs'
+import SectionDivider from "../../components/common/SectionDivider";
+import HowItWorkTwo from "../../components/hiw/hiw2/HowItWorkTwo";
+import RecommendedPlace from "../../components/places/RecommendedPlace";
+import FunFactsTwo from "../../components/other/funfacts/FunFactsTwo";
+import InfoBox3 from "../../components/other/infoboxes/InfoBox3";
+import Authors from "../../components/sliders/Authors";
 import Testimonial from "../../components/sliders/Testimonial";
+import LatestBlog from "../../components/blogs/LatestBlog";
+import CtaOne from "../../components/other/cta/CtaOne";
 import ClientLogo from "../../components/sliders/ClientLogo";
 import NewsLetter from "../../components/other/cta/NewsLetter";
 import Footer from "../../components/common/footer/Footer";
-import ScrollTopBtn from "../../components/common/ScrollTopBtn";
-import SectionsHeading from "../../components/common/SectionsHeading";
-import PopularCategories from "../../components/other/categories/PopularCategories";
-import HowItWorkOne from "../../components/hiw/hiw1/HowItWorkOne";
-import FunFactsOne from "../../components/other/funfacts/funfacts1/FunFactsOne";
-import CtaOne from "../../components/other/cta/CtaOne";
-import LatestBlog from "../../components/blogs/LatestBlog";
-import Button from "../../components/common/Button";
-import SectionDivider from "../../components/common/SectionDivider";
-import InfoBox3 from "../../components/other/infoboxes/InfoBox3";
-import PlaceOne from "../../components/places/PlaceOne";
 import sectiondata from "../../store/store";
 
-function Home() {
+function Home2() {
     return (
-        <main className="home-1">
+        <main className="home-2">
             {/* Header */}
             <GeneralHeader />
 
-            {/* Hero Banner */}
-            <BannerOne />
+            {/* Banner */}
+            <BannerTwo bgImg={sectiondata.herobanners.banner2.bgimage} />
 
-            {/* Popular Categories */}
-            <section className="cat-area padding-top-100px padding-bottom-90px">
+            {/* Popular Destination */}
+            <section className="cat-area destination-area padding-top-100px padding-bottom-100px">
                 <div className="container">
                     <div className="row section-title-width text-center">
-                        <SectionsHeading title={sectiondata.popularcategories.sectitle} desc={sectiondata.popularcategories.seccontent} />
+                        <SectionsHeading title={sectiondata.populardestination.sectitle} desc={sectiondata.populardestination.seccontent} />
                     </div>
+
                     <div className="row mt-5">
-                        <PopularCategories catitems={sectiondata.popularcategories.categories} />
+                        <PopularDestination destinations={sectiondata.populardestination.destinations} />
+                    </div>
+
+                    <div className="row">
+                        <div className="col-lg-12">
+                            <div className="button-shared mt-4 text-center">
+                                <Button text={sectiondata.populardestination.viewmorebtn} url={sectiondata.populardestination.viewmorebtnurl}>
+                                    <span className="la">
+                                        <BsEye />
+                                    </span>
+                                </Button>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </section>
 
-            {/* How It Work */}
-            <section className="hiw-area text-center padding-top-100px padding-bottom-110px">
+            <SectionDivider />
+
+            {/* How it Work */}
+            <HowItWorkTwo />
+
+            <SectionDivider />
+
+            {/* Recommended Place */}
+            <section className="card-area padding-top-100px padding-bottom-90px text-center">
                 <div className="container">
                     <div className="row section-title-width text-center">
-                        <SectionsHeading title={sectiondata.howitworks.hiw1.sectitle} desc={sectiondata.howitworks.hiw1.seccontent} />
+                        <SectionsHeading title={sectiondata.recommendedplaces.sectitle} desc={sectiondata.recommendedplaces.seccontent} />
                     </div>
-
-                    <HowItWorkOne hiw1content={sectiondata.howitworks.hiw1} />
-                </div>
-            </section>
-
-            {/* Most Visited Place */}
-            <section className="card-area text-center padding-bottom-100px">
-                <div className="container">
-                    <div className="row section-title-width text-center">
-                        <SectionsHeading title={sectiondata.mostvisitedplaces.sectitle} desc={sectiondata.mostvisitedplaces.seccontent} />
-                    </div>
-
-                    <PlaceOne places={sectiondata.mostvisitedplaces.places} />
+                    <RecommendedPlace recommendplaces={sectiondata.recommendedplaces.items} />
                 </div>
             </section>
 
             {/* FunFacts */}
             <section className="funfact-area section-bg-2 padding-top-100px padding-bottom-50px text-center">
                 <div className="container">
-                    <div className="row section-title-width">
-                        <SectionsHeading title={sectiondata.funfacts.funfact1.sectitle} titleClass="text-white" desc={sectiondata.funfacts.funfact1.seccontent} />
+                    <div className="row section-title-width text-center">
+                        <SectionsHeading title={sectiondata.funfacts.funfact2.sectitle} titleClass="text-white" desc={sectiondata.funfacts.funfact2.seccontent} />
                     </div>
 
-                    <FunFactsOne countitems={sectiondata.funfacts.funfact1.counteritems} />
+                    <FunFactsTwo funfactitems={sectiondata.funfacts.funfact2.counteritems} />
                 </div>
             </section>
 
@@ -79,39 +89,23 @@ function Home() {
                         <SectionsHeading title={sectiondata.howitworks.hiw2.sectitle} desc={sectiondata.howitworks.hiw2.seccontent} />
                     </div>
 
-                    <InfoBox3 infoitems={sectiondata.howitworks.hiw2.items} />
+                    <InfoBox3 infoitems={sectiondata.howitworks.hiw2.items} isbtnshow={true} />
                 </div>
             </section>
 
-            {/* CTA */}
-            <section className="cta-area section-bg column-sm-center padding-top-80px padding-bottom-80px">
-                {sectiondata.calltoactions.cta1.shapes.map((img, index) => {
-                    return (
-                        <img src={img.img} key={index} alt="Cta Symble" className="symble-img" />
-                    )
-                })}
-                <div className="container">
-                    <div className="row align-items-center">
-                        <div className="col-lg-9 text-left">
-                            <SectionsHeading title={sectiondata.calltoactions.cta1.title} titleClass=" mb-3 font-size-28" descClass=" font-size-17" desc={sectiondata.calltoactions.cta1.content} />
-                        </div>
-
-                        <div className="col-lg-3">
-                            <div className="btn-box">
-                                <Button text={sectiondata.calltoactions.cta1.btntext} url={sectiondata.calltoactions.cta1.btnurl} />
-                            </div>
-                        </div>
+            {/* Authors */}
+            <section className="author-area padding-top-100px padding-bottom-100px">
+                <div className="container-fluid">
+                    <div className="row section-title-width text-center">
+                        <SectionsHeading title={sectiondata.authors.sectitle} titleClass="text-white" desc={sectiondata.authors.seccontent} />
                     </div>
+
+                    <Authors authoritems={sectiondata.authors.sliders} />
                 </div>
             </section>
 
             {/* Testimonial */}
             <section className="testimonial-area padding-top-100px padding-bottom-100px text-center">
-                {sectiondata.testimonialdata.tmimage.map((tmimg, index) => {
-                    return (
-                        <img key={index} src={tmimg.tmimg} alt="testimonial" className="random-img" />
-                    )
-                })}
                 <div className="container">
                     <div className="row section-title-width text-center">
                         <SectionsHeading title={sectiondata.testimonialdata.sectitle} desc={sectiondata.testimonialdata.seccontent} />
@@ -159,8 +153,9 @@ function Home() {
             <Footer />
 
             <ScrollTopBtn />
+
         </main>
-    )
+    );
 }
 
-export default Home;
+export default Home2;

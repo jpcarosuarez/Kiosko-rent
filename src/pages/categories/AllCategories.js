d6a273bc54355c5dec981eb219d11922709cbaf8
smithclaudioanibal@gmail.com <smithclaudioanibal@gmail.com>
2021-04-09 13:36:44 -0300

    agregamos firebase

--- src/pages/categories/AllCategories.js
+++ src/pages/categories/AllCategories.js
@@ -8,11 +8,41 @@ import ScrollTopBtn from "../../components/common/ScrollTopBtn";
 import PopularCategoriesMore from "../../components/other/categories/PopularCategoriesMore";
 import breadcrumbimg from '../../assets/images/bogotaAndes.jpg'
 import sectiondata from "../../store/store";
+import {useEffect, useState} from 'react';
+import {getFirestore} from '../../api/db';
+import {GiChickenOven, GiPositionMarker, GiWineGlass} from 'react-icons/gi';
+import apartamentos from "../../assets/images/apartamentos.jpg"; // 263*175
+
 
 const state = {
     breadcrumbImg: breadcrumbimg,
 }
 function AllCategories() {
+
+    const db = getFirestore();
+    const [categorias, setCategorias] = useState([]);
+
+    useEffect(() => {
+        db.collection('categorias').get()
+            .then(response => {
+                let arr = [];
+                response.forEach(doc => {
+                    arr.push({
+                        id: doc.id,
+                        icon: <GiChickenOven />, //TODO: traer icono segun categoria
+                        title: doc.data().descripcion,
+                        stitle: '12 Listings', //TODO: número de publicaciones de la colección
+                        url: '#',
+                        img: apartamentos
+                    })
+                })
+                setCategorias(arr);
+                console.log(arr);
+            })
+        .catch(e => console.log(e))
+    });
+    
+
     return (
         <main className="all-categories">
             {/* Header */}
@@ -25,7 +55,8 @@ function AllCategories() {
                 <div className="container">
                     <div className="row">
                         <PopularCategories catitems={sectiondata.popularcategories.categories} />
-                        <PopularCategoriesMore catitems={sectiondata.popularcategories.morecats} />
+                        {/* <PopularCategories catitems={categorias} /> */}
+                        {/* (se quita y se muestran todas) <PopularCategoriesMore catitems={sectiondata.popularcategories.morecats} /> */}
                     </div>
                 </div>
             </section>

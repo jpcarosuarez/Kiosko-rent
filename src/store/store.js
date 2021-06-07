baa26b6e17f6734ed645782b42212ee0d940bed4
jpcarosuarez <46928724+jpcarosuarez@users.noreply.github.com>
2021-06-07 15:57:40 -0500

    WasiView

--- src/store/store.js
+++ src/store/store.js
@@ -158,7 +158,7 @@ const sectiondata = {
                 },
                 {
                     title: 'Mostrar todos',
-                    path: '/todos'
+                    path: '/categorias'
                 },
                 {
                     title: 'Publicar un Arriendo',
